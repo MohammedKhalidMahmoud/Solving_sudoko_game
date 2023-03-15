8a6b6ef4880bfac5ea93101d52df263c54f9ae80
mohamedkhaled <76552489+MohammedKhalidMahmoud@users.noreply.github.com>
2023-03-15 14:29:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <cmath>
-#define N 4
+#define N 9
 using namespace std;
-
-
 
  bool isSafe(int grid[N][N],int i,int j,int c){
   for(int k=0;k<N;k++){
@@ -40,7 +38,7 @@ bool solve(int grid[N][N]){
         grid[i][j]=c;
         if(solve(grid))
             return true;
-        //grid[i][j]=0;
+        grid[i][j]=0;
     }
  }
  return false;
@@ -58,10 +56,15 @@ int main()
 {
     //int N=4;
     int grid[N][N]={
-    {1,0,3,0},
-    {0,0,2,1},
-    {0,1,0,2},
-    {2,4,0,0}   // i=N & j=N
+        {0,0,0,0,7,0,0,0,0},
+        {0,9,0,5,0,6,0,8,0},
+        {0,0,8,4,0,1,2,0,0},
+        {0,5,9,0,0,0,8,4,0},
+        {7,0,0,0,0,0,0,0,6},
+        {0,2,3,0,0,0,5,7,0},
+        {0,0,5,3,0,7,4,0,0},
+        {0,1,0,6,0,8,0,9,0},
+        {0,0,0,0,1,0,0,0,0}  // i=N & j=N
     };
     if (solve(grid)){
         print_grid(grid);
@@ -71,54 +74,41 @@ int main()
     return 0;
 }
 
+
+// Sudoko Problems
+
 /*
+    {5,3,0,0,7,0,0,0,0},
+    {6,0,0,1,9,5,0,0,0},
+    {0,9,8,0,0,0,0,6,0},
+    {8,0,0,0,6,0,0,0,3},
+    {4,0,0,8,0,3,0,0,1},
+    {7,0,0,0,2,0,0,0,6},
+    {0,6,0,0,0,0,2,8,0},
+    {0,0,0,4,1,9,0,0,5},
+    {0,0,0,0,8,0,0,7,9}
+*/
 
-bool isSafe(int grid[N][N],int row,int col){
-  for(int i=0;i<col;i++)
-    if(grid[row][i]==1) return false;
+/*
+    {0,0,0,0,7,0,0,0,0},
+    {0,9,0,5,0,6,0,8,0},
+    {0,0,8,4,0,1,2,0,0},
+    {0,5,9,0,0,0,8,4,0},
+    {7,0,0,0,0,0,0,0,6},
+    {0,2,3,0,0,0,5,7,0},
+    {0,0,5,3,0,7,4,0,0},
+    {0,1,0,6,0,8,0,9,0},
+    {0,0,0,0,1,0,0,0,0}
+*/
 
-  for(int i=row,j=col; i>=0 && j>=0; i--,j--)
-    if(grid[i][j]==1) return false;
-
-  for(int i=row,j=col; i<N && j>=0; i++,j--)
-    if(grid[i][j]==1) return false;
-
-  return true;
-}
-
-bool solve(int grid[N][N],int col){
-    if(col==N) return true;
-    for(int i=0;i<N;i++){
-        if(isSafe(grid,i,col)){
-           grid[i][col]=1;
-           if(solve(grid,col+1)) return true;
-           // grid[i][col]=0;
-    }
-}
- return false;
-}
-
-void print_grid(int grid[N][N]){
-  for(int i=0;i<N;i++){
-    for(int j=0;j<N;j++){
-        cout<<grid[i][j]<<" ";
-    }
-    cout<<"\n";
-  }
-}
-int main(){
-    int grid[N][N]={
-        {0,0,0,0},
-        {0,0,0,0},
-        {0,0,0,0},
-        {0,0,0,0}
-    };
-if(solve(grid,0)){
-    print_grid(grid);
-}
-else
-    cout<<"There is no solution";
-return 0;
-}
-
+/*
+    {6,0,0,0,0,9,0,0,0},
+    {0,0,0,0,0,0,0,0,4},
+    {0,0,7,6,4,8,0,0,2},
+    {0,0,2,0,5,0,8,0,0},
+    {0,0,1,8,0,0,0,2,3},
+    {7,0,4,0,0,0,0,0,0},
+    {0,0,0,3,0,0,0,4,0},
+    {0,0,0,0,6,0,5,0,0},
+    {0,2,3,0,1,0,0,0,0}
 */
